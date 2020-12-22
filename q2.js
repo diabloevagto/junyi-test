d98474ca98d9182a73d344139677d5f1e5985574
diabloevagto <diabloevagto@gmail.com>
2020-12-22 17:48:01 +0800

    feat: q2

--- q2.js
+++ q2.js
@@ -1,1 +1,6 @@
-// empty
+const Q2 = (target) =>
+  Array.from(Array(target).keys(), (val) => val + 1).filter(
+    (v) => (v % 3 != 0 && v % 5 != 0) || (v % 3 === 0 && v % 5 === 0)
+  );
+
+console.log(Q2(15).join(", "));
