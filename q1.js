76af90038784154edc7dcbd93d14ca70b0b636a3
diabloevagto <diabloevagto@gmail.com>
2020-12-22 17:42:47 +0800

    feat: q1

--- q1.js
+++ q1.js
@@ -1,1 +1,10 @@
-// empty
+const Q1_A = (st) => st.split("").reverse().join("");
+
+const Q1_B = (st) =>
+  st
+    .split(" ")
+    .map((v) => Q1_A(v))
+    .join(" ");
+
+console.log(Q1_A("junyiacademy"));
+console.log(Q1_B("flipped class room is important"));
